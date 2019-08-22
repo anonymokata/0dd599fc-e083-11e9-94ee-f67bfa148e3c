ce3fa765e62dece470ca708340168687cd687e0e
someone <someone>
2019-08-21 21:22:48 -0400

    Word Search - Adding File Read

---
+++ EHale.Wordsearch.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using EHale.Wordsearch.Entities;
+using System;
+
+namespace EHale.Wordsearch.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle puzzle = new Puzzle(@"C:\temp\puzzle.txt");
+            string[] results = puzzle.Solve();
+
+            foreach(string result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EHale.Wordsearch.Entities/Puzzle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace EHale.Wordsearch.Entities
+{
+    public class Puzzle
+    {
+        private SearchBoard _board;
+        private string[] _words;
+
+        public Puzzle(string path)
+        {
+            LoadFromFile(path);
+        }
+
+        public string[] Solve()
+        {
+            List<string> results = new List<string>();
+
+            foreach(string word in _words)
+            {
+                results.Add(_board.Find(word));
+            }
+
+            return results.ToArray();
+        }
+
+        private void LoadFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            _words = lines[0].Split(',');
+            List<string> boardRows = new List<string>();
+
+            for(int fileRow = 1; fileRow < lines.Length; fileRow++)
+            {
+                boardRows.Add(lines[fileRow]);
+            }
+
+            _board = new SearchBoard(boardRows.ToArray());
+        }
+    }
+}
